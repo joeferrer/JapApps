ee7f7b8e52c806d8c4bf32c53180dbbd1a2549be
joeferrer <joferrer16@gmail.com>
2014-04-14 23:21:19 +0800

    edited calendar.py

--- python/calendar.py
+++ python/calendar.py
@@ -7,22 +7,8 @@
 
 from random import randrange
 from operator import add
-import sys
-
-def get_input():
-	version = sys.version_info
-	if version[0] > 2:
-		return input()
-	else:
-		return raw_input()
-
-def get_game_mode():
-	choice = "z"
-	while choice.lower() != "a" and choice.lower() != "b":
-		choice = get_input()
-		if choice.lower() != "a" and choice.lower() != "b":
-			print "Invalid input. Choose between 'A' and 'B' only!"
-	return choice
+from modules.userinput import get_input
+from modules.userinput import get_game_mode
 
 def str_dayn(x):
 	r = x%10
