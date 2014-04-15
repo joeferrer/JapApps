89005b6fff9caaa1ec5652f25b242c8ae3bd4a68
joeferrer <joferrer16@gmail.com>
2014-04-15 20:07:07 +0800

    updated user_end module

--- python/modules/user_end.py
+++ python/modules/user_end.py
@@ -15,3 +15,11 @@ def get_game_mode():
 		if choice.lower() != "a" and choice.lower() != "b":
 			print "Invalid input. Choose between 'A' and 'B' only!"
 	return choice
+
+def get_word_type():
+	wtype = "z"
+	while wtype.lower() != "h" and wtype.lower() != "f" and wtype.lower() != "hf" and wtype.lower() != "fh":
+		wtype = get_input()
+		if wtype.lower() != "h" and wtype.lower() != "f" and wtype.lower() != "hf" and wtype.lower() != "fh":
+			print "Invalid input. Choose among 'H','F','FH' and 'HF' only!"
+	return wtype
